bee21073d8bc71b66202024a071e5c38192df76d
Optivify <optivify@github.com>
2022-12-26 22:54:30 +0700

    Add benchmark project

---
+++ src/Benchmarks/Optivify.BrowserDetection.Benchmark/Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkDotNet.Running;
+using Optivify.BrowserDetection.Benchmark;
+
+var run = BenchmarkRunner.Run<DetectionServiceBenchmarks>();
+Console.WriteLine($"Benchmark done in: {run.TotalTime.TotalMilliseconds}ms.");
+Console.ReadLine();
